3b9a91b89434439425be140e28dcf128364f04b4
ottertoss <ottertoss@users.noreply.github.com>
2014-11-15 14:18:45 +0000

    Update cachematrix.R
    My changes to the coursework. Tested using the suggested tests.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to return the cached matrix and it's inverse. 
 
-## Write a short comment describing this function
-
+## Function to cache a matrix and return it and it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Sets the cache value.
+  set <- function(y) {
+    x <<- y
+  }
+    
+  ## Methods to return the matrix and getinverse
+  get <- function() x
+  getinverse <- function() solve(x)
+  
+  ## List of methods in function
+  list (get = get, set = set, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Function to return the solved matrix as it's inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  x$getinverse()
 }
